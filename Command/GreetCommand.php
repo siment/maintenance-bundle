ae302409fd0d61e423de9abc516d284bd8abe823
Simen Thorsrud <simen.thorsrud@gmail.com>
2019-11-29 00:21:55 +0100

    Added boilerplate command and adjusted code fixer rules

---
+++ Command/GreetCommand.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Copyright (C) 2019 Simen Thorsrud
+ * @author Simen Thorsrud <simen.thorsrud@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Siment\MaintenanceBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * A simple demo command that outputs a greeting.
+ */
+class GreetCommand extends Command
+{
+    /** @var string The name of the command */
+    protected static $defaultName = 'demo:greet';
+
+    /**
+     * Configuring the command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Greet someone')
+            ->setHelp('Saying hello')
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            )
+        ;
+    }
+
+    /**
+     * Executing the command.
+     *
+     * @param InputInterface  $input  Input to command
+     * @param OutputInterface $output Output from command
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+
+        return 0;
+    }
+}
