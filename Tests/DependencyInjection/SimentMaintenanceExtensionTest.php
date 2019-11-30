ce3075ee78960e3083cff557599c292bdef18903
Simen Thorsrud <simen.thorsrud@gmail.com>
2019-11-30 13:17:37 +0100

    Now testing dependency injection

---
+++ Tests/DependencyInjection/SimentMaintenanceExtensionTest.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (C) 2019 Simen Thorsrud
+ * @author Simen Thorsrud <simen.thorsrud@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Siment\MaintenanceBundle\DependencyInjection\SimentMaintenanceExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class SimentMaintenanceExtensionTestTestCase extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions(): array
+    {
+        return [new SimentMaintenanceExtension()];
+    }
+
+    public function testLoad()
+    {
+        $this->load();
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\Command\MaintenanceDisableCommand');
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\Command\MaintenanceEnableCommand');
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\Command\MaintenanceStatusCommand');
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\EventListener\RequestListener');
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\ModeManager\MaintenanceModeManager');
+        $this->assertContainerBuilderHasService('Siment\MaintenanceBundle\ModeManager\MaintenanceModeManagerInterface');
+    }
+}
